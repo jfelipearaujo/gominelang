012df54a696c729348369b0c8ab9ee8187f4123c
Jose Felipe Araujo <j.felipe.araujo@outlook.com>
2024-12-15 22:55:52 -0300

    chore: fixed build flag replacement

--- Makefile
+++ Makefile
@@ -52,7 +52,7 @@ run:
 	go run ./cmd/main.go
 
 build:
-	go build -race -o ./bin/gominelang -ldflags="-s -w" ./cmd/main.go
+	CGO_ENABLED=0 go build -o ./bin/gominelang -ldflags="-s -w -X main.GoMineLangVersion=v1.0.0" ./cmd/main.go
 
 install: build
 	go install ./cmd/main.go
--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
+	"time"
 
 	"github.com/jfelipearaujo/gominelang/internal/application/services/base_lang"
 	"github.com/jfelipearaujo/gominelang/internal/application/services/config"
@@ -14,16 +16,50 @@ import (
 	"github.com/jfelipearaujo/gominelang/internal/application/services/translate_tag"
 )
 
-// GoMineLangVersion is the version of the cli to be overwritten by goreleaser in the CI run with the version of the release in github
+// GoMineLangVersion is the version of the CLI to be overwritten by goreleaser in the CI run with the version of the release in github
 var GoMineLangVersion string
 
 const (
 	CONFIG_FILE_NAME string = ".gominelang.yaml"
 )
 
+func getVersion() string {
+	noVersionAvailable := "No version info available for this build"
+
+	if len(GoMineLangVersion) > 0 {
+		return GoMineLangVersion
+	}
+
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return noVersionAvailable
+	}
+
+	if bi.Main.Version != "(devel)" {
+		return bi.Main.Version
+	}
+
+	var vcsRevision string
+	var vcsTime time.Time
+	for _, setting := range bi.Settings {
+		switch setting.Key {
+		case "vcs.revision":
+			vcsRevision = setting.Value
+		case "vcs.time":
+			vcsTime, _ = time.Parse(time.RFC3339, setting.Value)
+		}
+	}
+
+	if vcsRevision != "" {
+		return fmt.Sprintf("%s (%s)", vcsRevision, vcsTime)
+	}
+
+	return noVersionAvailable
+}
+
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "version" {
-		fmt.Printf("Version: %s\n", GoMineLangVersion)
+		fmt.Printf("Version: %s\n", getVersion())
 		os.Exit(0)
 	}
 
